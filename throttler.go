1cc37227f05edf290d238f65f01ba92b18662dea
David Bariod <david.bariod@qonto.eu>
2019-12-15 15:24:46 +0100

    initial commit

---
+++ throttler.go
@@ -0,0 +1,46 @@
+// package throttler implements throttling on Runner interface.
+package throttler
+
+import (
+	"container/ring"
+	"time"
+)
+
+type Throttler struct {
+	period       time.Duration
+	numOccurence uint
+	r            *ring.Ring
+}
+
+func New(period time.Duration, numOccurence uint) *Throttler {
+	if numOccurence == 0 {
+		return nil
+	}
+	r := ring.New(int(numOccurence))
+	t := &Throttler{
+		period:       period,
+		numOccurence: numOccurence,
+		r:            r,
+	}
+	return t
+}
+
+func (t *Throttler) needThrottle() bool {
+	if t.r.Next().Value == nil {
+		return false
+	}
+	if time.Now().Sub(*t.r.Next().Value.(*time.Time)) > t.period {
+		return false
+	}
+	return true
+}
+
+func (t *Throttler) Throttle() {
+	if t.needThrottle() {
+		time.Sleep(
+			t.r.Next().Value.(*time.Time).Add(t.period).Sub(time.Now()))
+	}
+	now := time.Now()
+	t.r = t.r.Next()
+	t.r.Value = &now
+}
---
+++ throttler_test.go
@@ -0,0 +1,34 @@
+package throttler_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dgsb/kipt/throttler"
+)
+
+func TestThrottler(t *testing.T) {
+	tt := throttler.New(1*time.Second, 5)
+
+	firstStart := time.Now()
+	for i := 0; i < 5; i++ {
+		start := time.Now()
+		tt.Throttle()
+		require.True(t, time.Now().Sub(start) < 10*time.Millisecond)
+	}
+
+	tt.Throttle()
+	require.True(t, time.Now().Sub(firstStart) >= time.Second)
+}
+
+func TestThrottle(t *testing.T) {
+	tt := throttler.New(250*time.Millisecond, 1)
+	firstStart := time.Now()
+	tt.Throttle()
+	require.True(t, time.Now().Sub(firstStart) < 10*time.Millisecond)
+	tt.Throttle()
+	require.True(t, time.Now().Sub(firstStart) > 250*time.Millisecond)
+	require.True(t, time.Now().Sub(firstStart) < 260*time.Millisecond)
+}
